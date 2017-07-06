5b864c5fa5a27bbaddf7e7deafeedb19f0e0e51d
possoumous <jwmorrow218@gmail.com>
2017-07-06 19:08:21 -0400

    Update Watchers.py

--- examples/Watchers.py
+++ examples/Watchers.py
@@ -4,7 +4,7 @@ from seleniumbase import BaseCase
 class MyTestClass(BaseCase):
 
     def test_basic(self):
-        self.open('https://stocktwits.com/')            # Navigate to the web page
+        self.open('https://stocktwits.com/symbol/CYTR?q=cytr')            # Navigate to the web page
         self.assert_element('sentiment-tab')       # Assert element on page
         self.click('sentiment-tab')                  # Click element on page
         
