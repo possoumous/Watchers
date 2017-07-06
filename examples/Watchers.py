d842e9dd82b59cca7f4534740db22a44c05652f2
possoumous <jwmorrow218@gmail.com>
2017-07-06 19:31:48 -0400

    Update Watchers.py

--- examples/Watchers.py
+++ examples/Watchers.py
@@ -1,5 +1,36 @@
 from seleniumbase import BaseCase
-
+import getpass
+import json
+import logging
+import math
+import os
+import pytest
+import sys
+import time
+import traceback
+import unittest
+import uuid
+from BeautifulSoup import BeautifulSoup
+from pyvirtualdisplay import Display
+from seleniumbase.config import settings
+from seleniumbase.core.application_manager import ApplicationManager
+from seleniumbase.core.s3_manager import S3LoggingBucket
+from seleniumbase.core.testcase_manager import ExecutionQueryPayload
+from seleniumbase.core.testcase_manager import TestcaseDataPayload
+from seleniumbase.core.testcase_manager import TestcaseManager
+from seleniumbase.core import browser_launcher
+from seleniumbase.core import log_helper
+from seleniumbase.fixtures import constants
+from seleniumbase.fixtures import page_actions
+from seleniumbase.fixtures import page_utils
+from seleniumbase.fixtures import xpath_to_css
+from selenium.common.exceptions import (StaleElementReferenceException,
+                                        TimeoutException)
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver import ActionChains
 
 class MyTestClass(BaseCase):
 
