3502a301488a0569d9bcd473b29b7c420f0e8d22
possoumous <jwmorrow218@gmail.com>
2017-07-06 19:23:48 -0400

    Update Watchers.py

--- examples/Watchers.py
+++ examples/Watchers.py
@@ -5,6 +5,23 @@ class MyTestClass(BaseCase):
 
     def test_basic(self):
         self.open('https://stocktwits.com/symbol/CYTR?q=cytr')            # Navigate to the web page
-        self.assert_element('sentiment-tab')       # Assert element on page
-        self.click('sentiment-tab')                  # Click element on page
+        self.assert_element(<a class="watchers-top" href="/symbol/CYTR/watchers"> 17,379 Watchers </a>)       # Assert element on page
+        self.get_text(<a class="watchers-top" href="/symbol/CYTR/watchers"> 17,379 Watchers </a>)                  # Click element on page
         
+     def get_text(self, '#two-col-side > a', by=By.CSS_SELECTOR,
+                 timeout=settings.SMALL_TIMEOUT):
+        if self.timeout_multiplier and timeout == settings.SMALL_TIMEOUT:
+            timeout = self._get_new_timeout(timeout)
+        self.wait_for_ready_state_complete()
+        time.sleep(0.01)
+        element = page_actions.wait_for_element_visible(
+            self.driver, selector, by, timeout)
+        try:
+            element_text = element.text
+        except StaleElementReferenceException:
+            self.wait_for_ready_state_complete()
+            time.sleep(0.06)
+            element = page_actions.wait_for_element_visible(
+                self.driver, selector, by, timeout)
+            element_text = element.text
+        return element_text
