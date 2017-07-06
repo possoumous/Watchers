740cf4e1a25533b4d3279a17e23b1ff9f6c13006
possoumous <jwmorrow218@gmail.com>
2017-07-06 19:04:45 -0400

    Update Watchers.py

--- examples/Watchers.py
+++ examples/Watchers.py
@@ -1,25 +1,10 @@
-Import openpyxl
 from seleniumbase import BaseCase
 
-los = []
-url = 'https://stocktwits.com/symbol/'
-workbook = openpyxl.load_workbook('Test.xlsx')
-worksheet = workbook.get_sheet_by_name(name = 'Sheet1') 
-for col in worksheet['A']:
-    los.append(col.value)
-los2 = []
-print(los)
 
 class MyTestClass(BaseCase):
 
-
-
-#for i in los:
-  # stocksite = url +i + '?q=' +i 
-   
-       #driver.get(stocksite) 
-       #driver.find_element_by_id('sentiment-tab').click()
-       #Bullish = driver.find_elements_by_css_selector('span.bullish:nth-child(1)')
-       #Sentiment =  [x.text for x in Bullish]
-       #los2.append(Sentiment[0])
-       
+    def test_basic(self):
+        self.open('stockstwits.com')            # Navigate to the web page
+        self.assert_element('sentiment-tab')       # Assert element on page
+        self.click('sentiment-tab')                  # Click element on page
+        
