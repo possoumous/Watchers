27fedc9edb113ee55ea5bd3401d528d5ee29c7fa
possoumous <jwmorrow218@gmail.com>
2017-07-09 23:51:55 -0500

    Update test.py

--- examples/test.py
+++ examples/test.py
@@ -1,2 +1,13 @@
-test = 1+1
-print(test)
+from seleniumbase import BaseCase
+print "hello"
+
+class MyTestClass(BaseCase):
+
+    def test_basic(self):
+        self.open('https://stocktwits.com/symbol/CYTR?q=cytr')            # Navigate to the web page
+        self.assert_element('a.watchers-top:nth-child(3)')       # Assert element on page
+       # Click on link with the text
+        self.assert_text('a.watchers-top:nth-child(3)')
+        header_text = self.get_text('a.watchers-top:nth-child(3)')   # Grab text from page element
+        return header_text
+        
