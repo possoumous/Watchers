8045f5ae3691c17a2c5140b6ed0b76ef48c62014
possoumous <jwmorrow218@gmail.com>
2017-07-11 10:56:31 -0500

    Update watching.py

--- watching.py
+++ watching.py
@@ -9,6 +9,7 @@ display.start()
 workbook = openpyxl.load_workbook('Test.xlsx')
 worksheet = workbook.get_sheet_by_name(name = 'Sheet1')
 
+los2 = []
 los = []
 url = 'https://stocktwits.com/symbol/'
 for col in worksheet['A']:
@@ -22,6 +23,10 @@ with contextlib.closing(webdriver.Chrome()) as driver:
         print(stocksite)
         driver.get(stocksite)   
         Watchers = driver.find_elements_by_css_selector('a.watchers-top:nth-child(3)')
-        Sentiment =  [x.text for x in Watchers]
+        Watching =  [x.text for x in Watchers]
+        for i in los2:
+            los2[j]=i.rstrip("WATCHERS")
+            j+=1
+        los2.append(Sentiment[0])
 print(Sentiment)
 #test
